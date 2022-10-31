83d578962e80cdb542e000c6e07760933aaeb935
Garrison Snow <snow.garrison@gmail.com>
2022-10-31 11:02:57 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.cispgroup.warehouse_stock_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.cispgroup.warehouse_stock_project to javafx.fxml;
+    exports com.cispgroup.warehouse_stock_project;
+}
