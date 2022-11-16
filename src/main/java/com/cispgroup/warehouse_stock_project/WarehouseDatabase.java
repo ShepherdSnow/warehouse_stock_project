8bb97c4e9dda768342a9e76f6c4a3fc5cddf30bf
Garrison Snow <snow.garrison@gmail.com>
2022-11-16 12:11:41 -0500

    Changes by Emily

---
+++ src/main/java/com/cispgroup/warehouse_stock_project/WarehouseDatabase.java
@@ -0,0 +1,22 @@
+package com.cispgroup.warehouse_stock_project;
+
+import java.util.HashMap;
+
+/**
+ class WarehouseDatabase -- This is the class that holds the actual data for our warehouse. This class contains a number
+ of superpowered lists that contain that data.
+ */
+public class WarehouseDatabase {
+
+    private HashMap<String, StockItem> items = new HashMap<String, StockItem>();
+    private HashMap<String, Customer> names = new HashMap<String, Customer>();
+    private HashMap<String, Order> orders = new HashMap<String, Order>();
+
+    protected WarehouseDatabase() {
+        //does nothing;
+    }
+
+
+
+
+}
